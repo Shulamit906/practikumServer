f268b20d36c74518922e6c2364a24671e1206a53
hitmachut <hitmachut@students.local>
2024-06-04 11:50:35 +0300

    first commit

---
+++ Core/DTOs/RoleEmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class RoleEmployeeDto
+    {
+        public int RoleId { get; set; }
+        public bool IsManagement { get; set; }
+        public DateTime StartDateRole { get; set; }
+
+    }
+}
---
+++ serverPraktykum.API/Models/RolePostModel.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+using static Core.Entities.Role;
+
+namespace API.Models
+{
+    public class RolePostModel
+    {
+        public string Name { get; set; }
+    }
+}
